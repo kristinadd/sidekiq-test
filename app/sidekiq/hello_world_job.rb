fd4377643623a389376faefb68735512bcfbf999
kristinadd <kristina@productboard.com>
2025-04-30 17:32:39 +0200

    Sidekiq options

--- app/sidekiq/hello_world_job.rb
+++ app/sidekiq/hello_world_job.rb
@@ -1,11 +1,15 @@
 class HelloWorldJob
   include Sidekiq::Job
 
-  sidekiq_options retry: 2
+  sidekiq_options retry: 1
 
   def perform(name)
     puts "Starting job for #{name}"
-    
+
+    puts "Loaded HelloWorldJob with retry: #{sidekiq_options_hash['retry']}"
+
+    puts 
+
     raise "Something went wrong!" if name == "fail"
     
     puts "Job finished successfully for #{name}"
